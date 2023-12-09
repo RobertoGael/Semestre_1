46cbfc1eb35415bea81c9dfadb38926268f8822f
RobertoGael <l23180777@slp.tecnm.mx>
2023-12-08 22:02:00 -0600

    Unidad 5

---
+++ Unidad 5/C#/Program1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace U5P1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("La suma es " + suma(14, 8));
+            Console.WriteLine("La resta es " + resta(16, 8));
+            Console.WriteLine("La multiplicacion es " + multi(7, 8));
+            Console.WriteLine("La division es " + div(13, 8));
+        }
+        public static int suma(int a, int b)
+        {
+            int s = a + b;
+            return s;
+        }
+        public static int resta(int a, int b)
+        {
+            int r = a - b;
+            return r;
+        }
+        public static int multi(int a, int b)
+        {
+            int m = a * b;
+            return m;
+        }
+        public static int div(int a, int b)
+        {
+            int d = a / b;
+            return d;
+        }
+    }
+}
---
+++ Unidad 5/C#/Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace U5P2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.WriteLine("Dame el valor para x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dame el valor para y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tu resultado de la ecuacion1 = " + ecuacion1(x, y));
+            Console.WriteLine("Tu resultado de la ecuacion2 = " + ecuacion2(x, y));
+        }
+
+        public static double ecuacion1(double x, double y)
+
+        {
+            double r;
+            r = Math.Pow(x, 2) + (2 * Math.Pow(y, 4));
+            return r;
+        }
+
+        public static double ecuacion2(double x, double y)
+        {
+            double r;
+            r = Math.Sqrt(Math.Pow(x, 4) + 5 * y);
+            return r;
+        }
+    }
+}
